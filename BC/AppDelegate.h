ec18a29d76ff2dbe48e9b82137c8da072289c219
jinbo0122 <lijinbo0122@gmail.com>
2014-11-03 17:04:54 +0800

    Initial Commit

---
+++ BC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BC
+//
+//  Created by Albert Lee on 11/3/14.
+//  Copyright (c) 2014 Albert Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BC
+//
+//  Created by Albert Lee on 11/3/14.
+//  Copyright (c) 2014 Albert Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
